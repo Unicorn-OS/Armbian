d026b507cb0876a4e39817d71ecd8ceba5489b71
🦄Pegasus (DarkStar☀) <darkstar@unisocial.net>
2023-06-16 19:05:15 -0500

    Vagrant

--- src/ansible/.pre/vagrant.py
+++ src/ansible/.pre/vagrant.py
@@ -1,3 +1,4 @@
+
 def init():
 
 def start():
@@ -9,3 +10,16 @@ def connection():
 
 def test():
 "ansible -m ping vagrant"
+
+def inventory():
+vagrant_ip = connection
+
+f'''
+[vagrant]
+vagrant_ip, private_key = connection()
+
+[vagrant:vars]
+ansible_user=vagrant
+ansible_ssh_private_key_file={private_key}
+'''
+file = /etc/ansible/hosts
